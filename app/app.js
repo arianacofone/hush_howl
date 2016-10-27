1ba2a4823a92dfee6f3fb9eadfeb1d572258f4f2
arianadziedzic <ariana.dziedzic@gmail.com>
2016-10-27 17:33:36 -0400

    uncomment code

--- app/app.js
+++ app/app.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-// const cookieParser = require('cookie-parser');
-// const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const registerRouter = require('./routes/registerRouter');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.use(cookieParser());
+app.use(cookieParser());
 
-// app.use(session({
-//   secret: process.env.SESSION_SECRET,
-//   cookie: {},
-//   resave: true,
-//   saveUninitialized: true,
-// }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: true,
+  saveUninitialized: true,
+}));
 
 app.use(morgan('dev'));
 
