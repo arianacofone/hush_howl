76a6f201e1131a5504239c5963ef39edd147209a
arianadziedzic <ariana.dziedzic@gmail.com>
2016-10-30 10:44:48 -0400

    update styling

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Modal, { closeStyle } from 'simple-react-modal';
+import Modal from 'simple-react-modal';
 import Registration from './Registration.jsx';
 
 class App extends Component {
@@ -19,21 +19,13 @@ class App extends Component {
       <div id="app">
         <h2>HUSH/HOWL</h2>
         <h4>AN EXPERIMENT IN THE EVERYDAY ABSURD</h4>
-        <Registration
-          openModal={this.show}
-        />
+        <Registration openModal={this.show} />
         <Modal
           className="modal"
           show={this.state.show}
         >
-          <div id="img">
-            <img src="http://freebiesfireworks.com/wp-content/uploads/2015/06/f6-1.png" />
-          </div>
-          <h1 id="signup">Welcome to the Family</h1>
-
+          <h3 id="signup">Welcome to the Family!</h3>
         </Modal>
-
-
       </div>
     );
   }
