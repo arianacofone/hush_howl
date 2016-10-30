514bfd361b8a50dd6df5f1d10b6191e13cc54e0e
arianadziedzic <ariana.dziedzic@gmail.com>
2016-10-30 11:34:18 -0400

    add server commands

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ if (process.env.ENV === 'dev') {
 }
 
 app.use(express.static(path.join(__dirname, '/dist')));
+app.use(fallback('index.html', { root: __dirname + '/dist'}));
+
 app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, 'dist/index.html'));
 });
